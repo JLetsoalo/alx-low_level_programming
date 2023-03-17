4e2d146ce7e13208085e4311a96b256c919cce4a
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-17 01:41:04 -0700

    Readme file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing out lines using printf function
+ * Return: programme will return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+Hello world c language first project
