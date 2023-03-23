8b129f6f9b9e01fc871ba24ae86eb24afc481cc1
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-23 14:09:43 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,12 @@
 /**
  *print_to_98 - prints natural numbers
  *@n: argument to hold integer value
+ *Description: natural numbers 1 to 98
  */
 
 void print_to_98(int n)
 {
-for (n = 0; n <=98: n++)
+for (n = 0; n <=98; n++)
 {
 if (n <= 9)
 {
