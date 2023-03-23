7d831378b30a2c4ed6422d635fed1e46812037e3
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-22 21:45:46 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - calculates absolute value
+ *@n: integer agrument
+ *Return: returns absolute value
+ */
+
+int _abs(int)
+{
+if (int > 0)
+return (int);
+else if (int < 0)
+return (-int);
+
+return (0);
+}
