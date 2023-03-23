98fac8e36079a6ea10b58f50ba02c7d3e6c92bfd
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-23 13:41:09 -0700

    jack bouer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *jack_bauer - print out time
+ *Main: computes time in minutes and hours
+ */
+
+void jack_bauer(void)
+{
+int t_hour = 0;
+int t_min = 0;
+while (t_hour < 24)
+{
+while (t_min <= 60)
+{
+_putchar('0' + (t_hour / 10));
+_putchar('0' + (t_hour % 10));
+_putchar(':');
+_putchar('0' + (t_min / 10));
+_putchar('0' + (t_min % 10));
+_putchar('\n');
+t_min++;
+}
+t_min = 0;
+t_hour++;
+}
+}
