6192374c698860d9d41e6055c552afe586cebeeb
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-22 20:06:51 -0700

    lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *print_alphabet - print lowercase alphabet
+ *Return: always 0
+ */
+
+void print_alphabet(void)
+{
+char alph, small;
+for (alph = 'A'; alph <= 'Z'; ++alph)
+{
+small = tolower(alph);
+_putchar(alph);
+}
+_putchat('\n');
+return (0);
+}
