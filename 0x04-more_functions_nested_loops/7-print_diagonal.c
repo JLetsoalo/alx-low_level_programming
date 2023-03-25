6f38060c06ba0ab3858e89397fb4bdc22c29295a
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-24 22:08:36 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,12 @@ void print_diagonal(int n)
 	{
 		for (draw = 1; draw <= n; draw++)
 		{
-		for (space = 1; space < draw; space++)
-		{
-			_putchar(' ');
+			for (space = 1; space < draw; space++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
