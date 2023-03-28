beaf1e6f00196080391f902b2b920fcb110a7edd
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-27 20:09:54 -0700

    3_puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: variable to hold string
+ * Return: always 0
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	
+	_putchar('\n');
+}
