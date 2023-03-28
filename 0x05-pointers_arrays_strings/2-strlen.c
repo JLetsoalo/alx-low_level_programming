8411b78fe8ef7d3a1c866357c618c2b5ccbd6b0e
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-27 20:02:05 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - function to get length of string
+ *@s: variable to hold value
+ *Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int longint = 0;
+	
+	while (*s != '\0')
+	{
+		longint++;
+		s++;
+	}
+}
