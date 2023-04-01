44c443d27b34b36b9301fe4e1cdb373c62c0885a
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-03-31 19:45:39 -0700

    arrays, pointers and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,11 @@
 
 /**
  * leet - changes letters to numbers
- * @p: array for letters
+ * @str: array for letters
  * Return: returns characters of p
  */
 
-char *leet(char *p)
+char *leet(char *str)
 {
 	int j;
 	int i;
@@ -14,14 +14,14 @@ char *leet(char *p)
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0; p[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] <= '\0'; j++)
 		{
-			if (p[i] == a[j])
-				p[i] = b[j];
+			if (str[i] == a[j])
+				str[i] = b[j];
 		}
 	}
 
-	return (p);
+	return (str);
 }
