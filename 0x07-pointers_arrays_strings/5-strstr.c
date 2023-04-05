ec5938889d9f1266b187bf7c7bfebc25fcefa900
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-04 21:06:00 -0700

    chess board

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = strpbrk(str, set);
+	{
+	if (result != NULL) {
+        return (int)(result - str);
+	}
+	else
+	{
+	    return -1;
+	}
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - create chessboard using strings
+ * @a: string
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
+}
