5eb1490a25992745073735f26b449c27cdfd6eeb
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-05 19:16:51 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - print string using puts
+ * @s: string
+ */
+
 void _puts_recursion(char *s)
 {
+	if (s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
 	_putchar(s);
 	s++;
-	_putchar("\n");
+
 	_puts_recursion(s);
 }
