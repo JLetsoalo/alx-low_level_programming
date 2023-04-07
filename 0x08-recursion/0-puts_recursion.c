21a05935c43931f59332d65158ee924cc7fecd2e
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-06 22:22:21 -0700

    putchar fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * _puts_recursion - print string using puts
  * @s: string
  */
