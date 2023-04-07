ea001c2c970433c79738e872ab49a316a3e68f3a
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-06 22:19:00 -0700

    putchar fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * factorial - finds factorial of n
  * @n: integer to find factorial of
  * Return: returns an integer
@@ -22,18 +9,14 @@ int _putchar(char c)
 
 int factorial(int n)
 {
-	if (n == 0)
+	if (n == 0 || n == 1)
 	{
-		_putchar('1');
+		return (n);
 	}
 	else if (n < 0)
 	{
 		return (-1);
 	}
 	else
-	{	
-		return (factorial(n) - 1);
-	}
-
-	n++;
+		return (n * factorial(n - 1));
 }
