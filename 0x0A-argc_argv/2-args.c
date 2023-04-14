19742eb8875ce4cd9e745645f558b48ff75dc7ab
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-13 19:01:49 -0700

    fix arithmetic opp

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+ * main - print all arguments passed
+ * @argc: arguments to print
+ * @argv: array to store arguments
+ * Return: always 0
+ */
+
 int main( int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 0; i < argc: i++)
 	{
-		printf("%s\n", argc)
+		printf("%s\n", argv)
 	}
 	return (0);
 }
