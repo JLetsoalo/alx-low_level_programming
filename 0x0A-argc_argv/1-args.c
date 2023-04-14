9dc3d7ea064ce5fadf5198c60150b7c9172828ba
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-13 18:31:41 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print number of arguments
+ * @argc: collect arguments
+ * @argv: array to store argument address
+ * Return: always 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
