c3b271f43b898f1aa4934e666c4215251dc0cf54
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-13 18:11:00 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point (print current program name)
+ * @argc: argument to get program name
+ * @argv: array to store program name
+ * Return: Always 0 (Successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+
+	return (0);
+}
