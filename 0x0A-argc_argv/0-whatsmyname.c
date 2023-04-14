9787b1d0b5a88ce41755a0dc63dd46f6727d74bc
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-04-13 18:24:22 -0700

    add main h header

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point (print current program name)
@@ -9,7 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 
 	return (0);
 }
