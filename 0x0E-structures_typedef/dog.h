7c72db02eecc0eedbfaa0871f7e35229c912f47f
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-05-01 14:57:45 -0700

    dog type update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * dog_p - Typedef for structure dog
  */
-typedef struct dog dog_p;
+typedef struct dog dog_t;
 
 /**
  * struct dog - structure for dog
@@ -22,7 +22,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_p *new_dog(char *name, float age, char *owner);
-void free_dog(dog_p *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
