6fb0ed4b8bcaa45f7e5ff74be9e7a9d1de604960
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-05-01 15:33:11 -0700

    first commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * compare - compares strings
+ * @head: head
+ * @tail: side b (tail)
+ */
+
+int compare(char *head, char *tail)
+{
+
+	if (head >= tail)
+		return (1);
+	if (*head == *tail)
+		return (compare(head + 1, tail - 1));
+
+	return (0);
+}
+
+/**
+ * _strlen - get length of string
+ * @s: string to get size of
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + (_strlen(s)));
+}
+
+/**
+ * is_palindrome - check if string is  palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	int length = _strlen(s);
+
+	return (compare(s, (s + length - 1)));
+}
