7d6a1da29e8402b6666680ffba58771bd7238f51
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-05-01 03:00:54 -0700

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - creating array of characters
+ * @size: size of the array
+ * @c: character to get
+ * Return: string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int x = 0;
+
+	if (size <= 0 || arr == NULL)
+		return (NULL);
+	while (x < size)
+	{
+		arr[x] = c;
+		x++;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+malloc anf free functions
