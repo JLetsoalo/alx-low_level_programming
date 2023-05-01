5195eada0034d739d3f8a7abd01caa93c295b794
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-05-01 15:46:07 -0700

    code fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,28 +2,19 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * _puts_recursion - print string using puts
  * @s: string
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+
+	_puts_recursion(s);
 }
