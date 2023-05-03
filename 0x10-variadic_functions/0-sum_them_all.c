99b9051c5112e5293eace2db5adc3335efd150c0
letsoalo johannes <letsoalojohannes6@gmail.com>
2023-05-03 01:21:09 -0700

    int declare fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,10 +18,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	va_start(args, n);
 
-	unsigned int sum = 0;
-	unsigned int x = 0;
+	unsigned int sum = 0, x;
 
-	for (; x < n; x++)
+	for (x = 0; x < n; x++)
 	{
 		sum += va_arg(args, int);
 	}
