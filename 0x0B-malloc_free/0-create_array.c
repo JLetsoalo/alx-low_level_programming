f66642ff0186941815762534ab5c5b6406d21e71
JLetsoalo <45739347+JLetsoalo@users.noreply.github.com>
2023-06-21 17:14:10 +0200

    Update on last commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creating array of characters
+ * @size: size of the array
+ * @c: character to get
+ * Return: string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int x = 0;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (size <= 0 || arr == NULL)
+		return (NULL);
+	while (x < (int)size)
+	{
+		arr[x] = c;
+		x++;
+	}
+
+	return (arr);
+}
