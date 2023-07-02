14e87901ee2982a7d8b75869f3f964422fbaa086
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-07-02 08:26:05 -0700

    code fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,16 +9,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-		return (0);
-
-
 	if (y < 0)
 		return (-1);
-
-
-	if (x == 1 || y == 0)
+	else if ((x == 1) || (y == 0))
 		return (1);
-
 	return (x * _pow_recursion(x, y - 1));
 }
