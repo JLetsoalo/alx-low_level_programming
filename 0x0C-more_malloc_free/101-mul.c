276c698a58dbf54c46d4cfabe7d9c1d1220879ad
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-07-02 08:03:48 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,43 +1,102 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 
-int is_valid_number(const char *num)
+#define ERR_MSG "Error"
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+
+int is_digit(char *s)
 {
-	while (*num)
+	int x = 0;
+
+	while (s[x])
 	{
-		if (!isdigit(*num))
+		if (s[x] < '0' || s[x] > '9')
 			return (0);
-		num++;
+		x++;
 	}
 	return (1);
 }
 
-int multiply(int num1, int num2) {
-    return num1 * num2;
+/**
+ * _strlen - returns length of string
+ * @s: string to check
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int b = 0;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+	return (b);
 }
+
+/**
+ * errors - handles errors for main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("Error\n");
-		return (98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	int result = multiply(num1, num2);
-	printf("%d\n", result);
-
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
-
