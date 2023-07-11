7de7cc6604bf0289a94fff015402b601d4e9ab65
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-07-11 16:07:00 -0700

    return old file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,44 +5,42 @@
 #include <elf.h>
 
 /**
- * print_elf_header - print header of elf file
- * @header: get elf header
+ * print_elf_header - print elf header
+ * @header: header to get
  */
 
 void print_elf_header(const Elf32_Ehdr *header)
 {
-    int i;
-    unsigned int entry_point = (header->e_ident[EI_DATA] == ELFDATA2LSB) ?                                  ((header->e_entry & 0xFF) << 24) | ((header->e_entry & 0xFF00) << 8) |                              ((header->e_entry >> 8) & 0xFF00) | ((header->e_entry >> 24) & 0xFF) :                              header->e_entry;
+	int i;
 
-    printf("Magic: ");
-    for (i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", (unsigned int)header->e_ident[i]);
-    }
-    printf("\n");
+	printf("Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");
 
-    printf("Class: %u\n", (unsigned int)header->e_ident[EI_CLASS]);
-    printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "Unknown");
-    printf("Version: %u\n", (unsigned int)header->e_ident[EI_VERSION]);
-    printf("OS/ABI: %u\n", (unsigned int)header->e_ident[EI_OSABI]);
-    printf("ABI Version: %u\n", (unsigned int)header->e_ident[EI_ABIVERSION]);
+	printf("Class: %d\n", header->e_ident[EI_CLASS]);
+	printf("Data: %d\n", header->e_ident[EI_DATA]);
+	printf("Version: %d\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
 
-    printf("Type: %u\n", (unsigned int)header->e_type);
-
-    printf("Entry point address: 0x%08x\n", entry_point);
+	printf("Type: %d\n", header->e_type);
+	printf("Entry point address: %u\n", header->e_entry);
 
 }
 
 /**
- * main - print header
- * @argc: first argument
- * @argv: argument value
- * Return: 0 whem successful
+ * main - main program to get elf header
+ * @argc: argument 1
+ * @argv: argument 2
+ * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
+	int fd = open(argv[1], O_RDONLY);
 	Elf32_Ehdr header;
-	FILE *file = fopen(argv[1], "rb");
 
 	if (argc != 2)
 	{
@@ -50,28 +48,31 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", argv[1]);
-        return (98);
-    }
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error opening file: %s\n", argv[1]);
+		return (98);
+	}
 
-    if (fread(&header, sizeof(header), 1, file) != 1) {
-        fprintf(stderr, "Error reading ELF header\n");
-        fclose(file);
-        return (98);
-    }
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		fprintf(stderr, "Error reading ELF header\n");
+		close(fd);
+		return (98);
+	}
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "Not an ELF file: %s\n", argv[1]);
-        fclose(file);
-        return (98);
-    }
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+			header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 ||
+			header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Not an ELF file: %s\n", argv[1]);
+		close(fd);
+		return (98);
+	}
 
-    print_elf_header(&header);
+	print_elf_header(&header);
 
-    fclose(file);
-    return (0);
+	close(fd);
+	return (0);
 }
