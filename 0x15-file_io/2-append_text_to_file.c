6a1a02a74518c45010fe3ea5e08b21de3013b64a
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-07-10 21:18:37 -0700

    first comm

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int leng;
+
+	for (leng = 0; str[leng] != '\0'; leng++)
+		;
+
+	return (leng);
+}
+
+/**
+ * append_text_to_file - appends contents only if file exists
+ * @filename: file to append to
+ * @text_content: content to append into file
+ * Return: 1 on success, -1 on error
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fldesc;
+	int n_wrote;
+
+	if (!filename)
+		return (-1);
+
+	fldesc = open(filename, O_WRONLY | O_APPEND);
+	if (fldesc == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fldesc);
+		return (1);
+	}
+
+	/* write content */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fldesc);
+		return (-1);
+	}
+
+	close(fldesc);
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+/**
+ * _cp - program to copy files
+ * @argc: test 1
+ * @argv: test 2
+ * return: 0 success, -1 failure
+ */
+
+int _cp(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	
+	char *file_from = argv[1];
+	char *file_to = argv[2];
+
+	int fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
