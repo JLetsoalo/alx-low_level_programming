2b709bdce4b31beda74cf1c98a5a0abdc2698ad4
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-08-20 21:13:51 -0700

    node fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t n_nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		n_nodes++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (n_nodes);
+	return (nodes);
